90db35a0ae30e12fc5dfda1f829d59921e47697e
macmac <macmac@MacBook-Pro-de-macmac.local>
2024-10-15 16:36:02 -0300

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,20 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macmac <macmac@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 22:05:46 by meandrad          #+#    #+#             */
-/*   Updated: 2024/10/14 22:53:05 by meandrad         ###   ########.fr       */
+/*   Updated: 2024/10/15 16:35:08 by macmac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h> 
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t index;
-	size_t 
+	size_t	index;
+	size_t	dst_len;
+	size_t	src_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	
+	index = 0;
+	if (dstsize <= dst_len)
+		return(dstsize + src_len);
+	while (src[index] != '\0' && dst_len + index < dstsize - 1)
+	{
+		dst[dst_len + index] = src[index];
+		index++;
+	}
+	dst[dst_len + index] = '\0';
+	return (dst_len + src_len);
 	
 }
+
+/*#include <stdio.h> 
+int main (void)
+{
+	char dst[10] = 'hello,';
+	const char src[] = ' world!';
+	size_t result;
+
+	result = ft_strlcat(dst, src, 10);
+	
+	printf("%d", result);
+}*/
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,32 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macmac <macmac@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:26:49 by meandrad          #+#    #+#             */
-/*   Updated: 2024/10/14 22:53:36 by meandrad         ###   ########.fr       */
+/*   Updated: 2024/10/15 16:25:27 by macmac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h> 
-size_t ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	index; 
 	
 	index = 0;
-	while (src[index] != '\0' && index < (size - 1))
+	if (dstsize > 0)
 	{
-		dst[index] = src[index];
-		index++;
+		while (src[index] != '\0' && index < (dstsize - 1))
+		{
+			dst[index] = src[index];
+			index++;
+		}
+		dst[index] = '\0';
 	}
-	dst[index] = '\0';
-	while(src[index] != '\0')
-	{
-		index++;
-	}
-	return (index);
+	return	(ft_strlen(src));
 }
 
 /* #include <stdio.h>
