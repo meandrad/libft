8d9a49566db26c230bf34efbaf925b845f0e3e63
Melissa Andrade Garcia Da Silva <meandrad@c3r13p5.42sp.org.br>
2024-10-19 18:01:53 -0300

    libft

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,11 +6,9 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 19:40:18 by meandrad          #+#    #+#             */
-/*   Updated: 2024/10/14 20:03:48 by meandrad         ###   ########.fr       */
+/*   Updated: 2024/10/19 18:01:31 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_toupper(int c);
 
 int	ft_toupper(int c)
 {
